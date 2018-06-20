da45d661ffd0887bc9b5bb4c94779cc74c964453
Christine Zhou <cazhou@berkeley.edu>
2018-06-19 20:01:19 -0700

    Implement project.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // TODO: Say hello in any way that you'd like!
+        System.out.println("Hello world!");
     }
 }
